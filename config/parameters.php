7227d6bd090f1c393683e4831fd22b0892a5333c
Clem <clem.chassaing@gmail.com>
2017-02-21 10:56:29 +0100

    config / header / modification de l'index

--- config/parameters.php
+++ config/parameters.php
@@ -1,8 +1,8 @@
 <?php
 
 $db_host = "localhost";
-$db_name = "airbnb";
+$db_name = "apcards";
 $db_user = "root";
 $db_pass = "root";
 
-$siteurl = "http://localhost:8888/DC2_Clem/eirbnb/"; /* url racine qui est sur la page d'accueil */
+$siteurl = "http://localhost:8888/DC2_Clem/atelier_dev/"; /* url racine qui est sur la page d'accueil */
