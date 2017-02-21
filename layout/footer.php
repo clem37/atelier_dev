81bc4813a4ff687ead11d19e0ad1e5acede011e7
Clem <clem.chassaing@gmail.com>
2017-02-21 10:58:42 +0100

    Sepration avec le footer

---
+++ layout/footer.php
@@ -0,0 +1,31 @@
+<footer>
+          <div class="grid">
+              <nav>
+                <ul>
+                  
+                  <li><a href="#">Accueil /</a></li>
+                    <li><a href="#">Partenaires /</a></li>
+                    <li><a href="#">Contact /</a></li>
+                    <li><a href="#">Plan du site /</a></li>
+                    <li><a href="#">Référencement /</a></li>
+                    <li><a href="#">Mentions Légales</a></li>
+                  </ul>
+                          </nav>
+              
+              <h1 class="hidden_text">LOGO</h1>
+              
+              <p>© Clémence Chassaing</p>
+              
+              
+          </div>
+          </footer>
+      
+          
+         
+          
+          <script src="js/script.js"></script>
+      </body>
+      
+      
+      
+</html>
