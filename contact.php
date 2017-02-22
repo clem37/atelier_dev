089d2c0c79d3c03c4bf473f068a4be369abf784d
Clem <clem.chassaing@gmail.com>
2017-02-22 15:01:09 +0100

    contact

--- contact.php
+++ contact.php
@@ -1,4 +1,5 @@
 <?php require_once 'model/database.php'; ?>
+<script src="https://www.google.com/recaptcha/api.js"></script>
 
 <form action="envoyer_mail.php" method="POST" id="form-create-message">
     <div>
@@ -11,15 +12,17 @@
     </div>
     <div>
     <label>Mail</label>
-    <input type="text" name="mail">
+    <input type="email" name="mail">
     </div>
     <div>
     <label>Message</label>
     <textarea name="message">Entrez votre message</textarea>
     </div>
+    <div class="g-recaptcha" data-sitekey="6Lc8hRYUAAAAALGuPIjooQRb6S0a_AYt5WeI8yKf"></div>
     <div>
     <input type="submit" value="envoyer">
     </div>
+    
 </form>
 
 <script src="js/jquery-1.11.3.min.js"></script>
